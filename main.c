dd4319d39f07a5676a61e9d408dc17a4b12d2d35
aabeben <bhasanudin@gmail.com>
2019-12-16 02:23:59 +0000

    using symbolic constants

--- main.c
+++ main.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
 
 int main(void) {
-  float lower, upper, step;
-  float fahrenheit, celcius;
+  int fahr;
 
-  lower = 0;
-  upper = 300;
-  step = 20;
-  
-  fahrenheit = lower;
-  printf("%s\t%s\n","Fahrenheit","Celcius");
-  while(fahrenheit <= upper){
-     celcius = (5.0/9.0) * (fahrenheit - 32);
-     printf("%.2f\t%.2f\n",fahrenheit,celcius);
-     fahrenheit = fahrenheit + step;
+  for(fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP){
+    printf("%d %6.1f\n", fahr, (5.0/9.0)*(fahr - 32));
   }
   return 0;
 }
